038aeb87d128cbda49ed045bb0dff8b80b1de957
PeterCusack <cusackpeter@gmail.com>
2016-04-08 17:05:46 -0700

    adding form focus again to trigger event

--- public/js/application.js
+++ public/js/application.js
@@ -239,7 +239,7 @@ startListeners = function(){
 	$(document).on("click", "datepicker", function(){
 		$(this).blur();		
 	});
-	
+
 	$(document).on("click", ".send-user-info", function(event){
 		event.preventDefault();
 		var passengersInfo = {};
@@ -254,10 +254,11 @@ startListeners = function(){
 		Model.sendStripPaymentDetails($(this).serializeArray()) // sends stripe form data to /stripe/charge
 	});
 	
-	var $viewportMeta = $('meta[name="viewport"]');
-		$('input, select, textarea').bind('focus blur', function(event) {
-		$viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
-	});
+	// var $viewportMeta = $('meta[name="viewport"]');
+	// 	$('input, select, textarea').bind('focus blur', function(event) {
+	// 	$viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
+	// });
+
 // this stuff needs to be cleaned up a lot
 	$(document).on("click", "#one-way", function(event){
 		event.preventDefault();
